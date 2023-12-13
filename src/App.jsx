ed9bece587d0e7a5ed0297c86432e636de94a322
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2023-12-14 01:22:41 +0600

    qr my profile

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import {QRCodeCanvas, QRCodeSVG} from 'qrcode.react';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <QRCodeCanvas value="https://www.facebook.com/Rezaualsunny/" />,
+    </>
+  )
+}
+
+export default App
